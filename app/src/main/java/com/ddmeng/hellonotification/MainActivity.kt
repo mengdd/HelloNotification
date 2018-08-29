1ad2302496ac59ba78a2d670a415b4f196487654
Dandan MENG <mengdandanno1@163.com>
2018-08-29 10:47:11 +0800

    send basic notifications

--- app/src/main/java/com/ddmeng/hellonotification/MainActivity.kt
+++ app/src/main/java/com/ddmeng/hellonotification/MainActivity.kt
@@ -1,12 +1,92 @@
 package com.ddmeng.hellonotification
 
-import android.support.v7.app.AppCompatActivity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.support.v4.app.NotificationCompat
+import android.support.v4.app.NotificationManagerCompat
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val CHANNEL_ID = "Channel ID"
+        const val BASIC_NOTIFICATION_ID = 1
+        const val LONG_NOTIFICATION_ID = 2
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        createNotificationChannel()
+        button_basic.setOnClickListener { showBasicNotification() }
+        button_long.setOnClickListener { showLongNotification() }
+    }
+
+    private fun showBasicNotification() {
+
+        // Create an explicit intent for an Activity in your app
+        val intent = Intent(this, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+        val mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_android_black_24dp)
+                .setContentTitle("Content Title")
+                .setContentText("Content Text")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                // Set the intent that will fire when the user taps the notification
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(false)
+        val notificationManager = NotificationManagerCompat.from(this)
+
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(BASIC_NOTIFICATION_ID, mBuilder.build())
+
+    }
+
+    private fun showLongNotification() {
+        // Create an explicit intent for an Activity in your app
+        val intent = Intent(this, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+        val mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_android_black_24dp)
+                .setContentTitle("My notification")
+                .setContentText("Content Text -------------------- Much longer text that cannot fit one line...")
+                .setStyle(NotificationCompat.BigTextStyle()
+                        .bigText("Big Text --------------------- Much longer text that cannot fit one line..."))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                // Set the intent that will fire when the user taps the notification
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(false)
+        val notificationManager = NotificationManagerCompat.from(this)
+
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(LONG_NOTIFICATION_ID, mBuilder.build())
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val description = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance)
+            channel.description = description
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager!!.createNotificationChannel(channel)
+        }
+        // It's safe to call this repeatedly because creating an existing notification channel performs no operation.
     }
 }
